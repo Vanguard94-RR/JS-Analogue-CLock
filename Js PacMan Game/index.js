bbfd026f0f133fdd41b26e5b4be55ec1554740be
Manuel Cortes <vanguard9417@gmail.com>
2021-11-20 23:16:05 -0600

    Added sound

--- Js PacMan Game/index.js
+++ Js PacMan Game/index.js
@@ -13,11 +13,11 @@ import soundGhost from "./sounds/eat_ghost.wav";
 // Dom Elements
 const gameGrid = document.querySelector("#game");
 const scoreTable = document.querySelector("#score");
-const startButton = document.querySelector("#start-button");
+const startButton = document.querySelector("#start-button"); 
 // Game constants
 const POWER_PILL_TIME = 10000; // ms
 const GLOBAL_SPEED = 80; // ms
-const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL); 
 // Initial setup
 let score = 0;
 let timer = null;
